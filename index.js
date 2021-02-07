72a201533dfc17c6cc83b98c8862a7566f81dbfa
Omar Ben <omaxlive3@gmail.com>
2021-02-07 15:56:21 +0100

    Create query for Posts and Authors

--- index.js
+++ index.js
@@ -2,34 +2,42 @@ const { ApolloServer, gql } = require('apollo-server');
 
 // Schema
 const typeDefs = gql`
-  type Item {
+  type Post {
     title: String
     detail: String
   }
+  type Author {
+    author: String
+  }
   type Query {
-    getItems: [Item]
+    getPosts: [Post]
+    getAuthors: [Author]
   }
 `;
 
-const items = [
+const posts = [
   {
     title: 'item 1',
     detail: 'detail 1',
+    author: 'author 1',
   },
   {
     title: 'item 2',
     detail: 'detail 2',
+    author: 'author 2',
   },
   {
     title: 'item 3',
     detail: 'detail 3',
+    author: 'author 3',
   },
 ];
 
 // Resolvers
 const resolvers = {
   Query: {
-    getItems: () => items,
+    getPosts: () => posts,
+    getAuthors: () => posts,
   },
 };
 
