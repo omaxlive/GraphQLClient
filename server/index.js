0e90ea21374291e7190308a41b7593de99d41dfa
Omar Ben <omaxlive3@gmail.com>
2021-02-14 13:57:52 +0100

    Configure connection to mongodb.com

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./db/schema');
 const resolvers = require('./db/resolvers');
+const connectDB = require('./config/db');
+
+// Connect DB
+connectDB();
 
 const server = new ApolloServer({
   typeDefs,
