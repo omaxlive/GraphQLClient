95b6775465ce8dafc5bef90b36bec434830f7214
Omar Ben <omaxlive3@gmail.com>
2021-02-14 18:17:10 +0100

    Hash password

--- server/db/resolvers.js
+++ server/db/resolvers.js
@@ -16,7 +16,7 @@ const posts = [
   },
 ];
 const User = require('../models/users');
-
+const bcryptjs = require('bcryptjs');
 // Resolvers
 const resolvers = {
   Query: {
@@ -29,11 +29,15 @@ const resolvers = {
   },
   Mutation: {
     newUser: async (_, { input }) => {
-      const { email } = input;
+      const { email, password } = input;
       const userExists = await User.findOne({ email });
       if (userExists) {
         throw new Error('User exists');
       }
+
+      //Hash password
+      const salt = await bcryptjs.genSalt(10);
+      input.password = await bcryptjs.hash(password, salt);
 
       try {
         const user = new User(input);
