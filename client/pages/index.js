0741ad2b523b8c07f3edfc783b7b29b82fca51a3
Omar Ben <omaxlive3@gmail.com>
2021-02-16 22:54:28 +0100

    Add support for Typescript in next

--- client/pages/index.js
+++
@@ -1,9 +0,0 @@
-import Layout from "../layout/layout";
-
-export default function Home() {
-  return (
-    <Layout>
-      <h1 className="text-2xl text-gray-800 font-light">Home (index)</h1>
-    </Layout>
-  );
-}
