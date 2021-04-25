7d8da3deaae414da40ebc95fbe7aee513fa65346
Omar Ben <omaxlive3@gmail.com>
2021-04-25 16:28:29 +0200

    Prepare for deployment

--- server/index.js
+++ server/index.js
@@ -33,6 +33,6 @@ const server = new ApolloServer({
 }
 });
 
-server.listen().then(({ url }) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`Server ready in the URL ${url}`);
 });
