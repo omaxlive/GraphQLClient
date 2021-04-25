341f88f1d7042f5864fad955599fd66491bfe47a
Omar Ben <omaxlive3@gmail.com>
2021-04-26 00:32:11 +0200

    Text format

--- server/index.js
+++ server/index.js
@@ -5,32 +5,28 @@ const connectDB = require('./config/db');
 const jwt = require('jsonwebtoken');
 require('dotenv').config({ path: '.env' });
 
-
 // Connect DB
 connectDB();
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({req}) => {
-    // console.log(req.headers['authorization'])
-
-    console.log(req.headers);
-
+  context: ({ req }) => {
+    console.log('headers: ', req.headers);
     const token = req.headers['authorization'] || '';
-    if(token) {
-        try {
-            const user = jwt.verify(token.replace('Bearer ', ''), process.env.SECRET );
-            // console.log(usuario);
-            return {
-              user
-            }
-        } catch (error) {
-            console.log('Something went wrong');
-            console.log(error);
-        }
+    if (token) {
+      try {
+        const user = jwt.verify(token.replace('Bearer ', ''), process.env.SECRET);
+        // console.log(usuario);
+        return {
+          user,
+        };
+      } catch (error) {
+        console.log('Something went wrong');
+        console.log(error);
+      }
     }
-}
+  },
 });
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
--- server/models/Product.js
+++ server/models/Product.js
@@ -1,25 +1,25 @@
 const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    stock: {
-        type: Number,
-        required: true,
-        trim: true
-    },
-    price: {
-        type: Number,
-        required: true,
-        trim: true
-    },
-    created: {
-        type: Date,
-        default: Date.now()
-    }
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  stock: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  created: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
 productSchema.index({ nombre: 'text' });
